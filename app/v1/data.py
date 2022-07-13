cbae6d49a5a5383243d424529f189097baacd690
William Cox <williamcox@college.harvard.edu>
2022-07-13 09:43:00 -0400

    wip, file download works but read_feather afterward doesnt: Converting unsigned dictionary indices to pandas not yet supported

--- app/v1/data.py
+++ app/v1/data.py
@@ -3,7 +3,11 @@ from typing import cast, Optional
 
 import pandas as pd
 import structlog
+import tempfile
+
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import FileResponse
+from pyarrow.feather import write_feather
 
 from app import utils
 from crawler.utils import sanitize_table_path
@@ -73,22 +77,51 @@ def data_for_etl_variable(
 ):
     """Fetch data for a single variable."""
 
-    con = utils.get_readonly_connection(threading.get_ident())
+    query = _query_for_etl_variable(channel, namespace, version, dataset, table, limit)
+    df = cast(pd.DataFrame, query.fetch_df())
+    # TODO: converting to lists and then ormjson is slow, we could instead
+    # convert to numpy arrays on which ormjson is super fast
+    return df.to_dict(orient="list")
 
+
+@router.get(
+    "/dataset/feather/{channel}/{namespace}/{version}/{dataset}/{table}",
+    response_class=FileResponse,
+)
+def feather_for_etl_variable(
+    channel: str,
+    namespace: str,
+    version: str,
+    dataset: str,
+    table: str,
+    limit: int = 1000000000,
+):
+    """Fetch data for a single variable in feather format."""
+    query = _query_for_etl_variable(channel, namespace, version, dataset, table, limit)
+    filename = 'asdf.feather'  #TODO
+    write_feather(query.fetch_arrow_table(), filename, compression='zstd')
+    return filename
+
+
+def _query_for_etl_variable(
+        channel: str,
+        namespace: str,
+        version: str,
+        dataset: str,
+        table: str,
+        limit: int):
+    con = utils.get_readonly_connection(threading.get_ident())
     table_db_name = sanitize_table_path(
         f"{channel}/{namespace}/{version}/{dataset}/{table}"
     )
-
     q = f"""
     select
         *
     from {table_db_name}
     limit (?)
     """
-    df = cast(pd.DataFrame, con.execute(q, parameters=[limit]).fetch_df())
-    # TODO: converting to lists and then ormjson is slow, we could instead
-    # convert to numpy arrays on which ormjson is super fast
-    return df.to_dict(orient="list")
+    query_result = con.execute(q, parameters=[limit])
+    return query_result
 
 
 def _assert_single_variable(n, variable_id):
